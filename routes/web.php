a9e3f30e63c93a836f94c15466b07471d9d915c9
Hao Wang <89629020+C-x-3-3-0@users.noreply.github.com>
2023-03-02 09:24:36 +0100

    finished routing, updated homescreen

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,60 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('homepage');
+});
+Route::get('/browse', function () {
+    return view('browse');
+});
+
+Route::get('/commission-view', function () {
+    return view('commission-view');
+});
+
+Route::get('/contact-gamehelper', function () {
+    return view('contact-gamehelper');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/create-commission', function () {
+    return view('create-commission');
+});
+
+Route::get('/edit-commission', function () {
+    return view('edit-commission');
+});
+
+Route::get('/edit-profile', function () {
+    return view('edit-profile');
+});
+
+Route::get('/forgot-password', function () {
+    return view('forgot-password');
+});
+
+Route::get('/info', function () {
+    return view('info');
+});
+
+Route::get('/profile', function () {
+    return view('profile');
+});
+
+ /*Route::get('/reset-password', function () {
+    return view('reset-password');
+}); */
+
+Route::get('/sign-in', function () {
+    return view('sign-in');
+});
+
+/* Route::get('/sign-up', function () {
+    return view('sign-up');
+}); */
+
+Route::get('/team', function () {
+    return view('team');
 });
