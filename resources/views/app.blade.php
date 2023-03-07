f1d0720ef5d7f0d6cfcf89e24bd9d4b5f5c949b3
Hao Wang <89629020+C-x-3-3-0@users.noreply.github.com>
2023-03-07 11:30:14 +0100

    working on header and footer
    Co-authored-by: E-Nagy <E-Nagy@users.noreply.github.com>

---
+++ resources/views/app.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+
+<body>
+
+    <header>
+        <ul>
+            <li style="float:left"><a href="/">Home<img src="{{ asset('images/icon.png') }}"> </a></li>
+            <li class="center"><a href="/info">Info</a></li>
+            <li class="center"><a href="/team">Team</a></li>
+            <li style="float:right"><a href="/sign-in"><img src="{{ asset('images/icon.png') }}"></a></li>
+        </ul>
+    </header>
+
+    <footer>
+        <div class="footer-item">
+            <span>GameHelper</span>
+        </div>
+        <div class="footer-item">
+
+        </div>
+        <div class="footer-item">
+
+        </div>
+    </footer>
+</body>
+
+</html>
--- resources/views/sign-up.blade.php
+++ resources/views/sign-up.blade.php
@@ -3,38 +3,41 @@
 
 <head>
     <title>Sign up</title>
-    <link rel="stylesheet" href="{{ asset('css/sign-up.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/sign-in-up.css') }}">
 </head>
 
 <body class="background">
     <div class="body-container">
         <div class="form-container">
-            <form action="submit-form.php" class="form" method="POST">
+            <form action="/" class="form" method="POST">
 
                 <h1 class="title">Sign up</h1>
 
-                <label for="username" class="label">Username:</label>
-                <input type="text" id="username" name="username" placeholder="Username" required>
+                <label for="username" class="label">Username</label><br>
+                <input type="text" id="username" name="username" placeholder="Username" required><br>
 
-                <label for="email" class="label">Email:</label>
-                <input type="email" id="email" name="email" placeholder="Email" required>
+                <label for="email" class="label">Email</label><br>
+                <input type="text" id="email" name="email" placeholder="Email" required><br>
 
-                <label for="password" class="label">Password:</label>
-                <input type="password" id="password" name="password" placeholder="password" required>
 
-                <label for="passwordrepeat" class="label">Repeat password</label>
-                <input type="password" id="passwordrepeat" name="passwordrepeat" placeholder="password" required>
-                <button type="button" onclick="location.href='/sign-up'">Sign up</button>
+                <label for="password" class="label">Password</label><br>
+                <input type="password" id="password" name="password" placeholder="Password" required><br>
+
+                <button type="submit" href="/">Sign up</button>
+                <!--TODO: redirect to profile view-->
+
             </form>
+
             <div class="sign-switch">
-                <span class="button-label">Already have an Account?</span>
-                <button type="button" onclick="location.href='/sign-in'">Sign in</button><br><br>
+                <span class="button-label">Already have an account?</span>
+                <button type="button" onclick="location.href='/sign-in'">Sign in</button>
             </div>
         </div>
         <div class="image-container">
             <img src="{{ asset('images/sign-up.png') }}">
         </div>
     </div>
+
 
 </body>
 
