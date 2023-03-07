e2a077f30bc1d28eef80f7afe2457dd6651f4d31
Hao Wang <89629020+C-x-3-3-0@users.noreply.github.com>
2023-03-07 10:09:03 +0100

    finished the sign in page and working on sign up
    Co-authored-by: E-Nagy <E-Nagy@users.noreply.github.com>

--- resources/views/sign-up.blade.php
+++ resources/views/sign-up.blade.php
@@ -7,29 +7,33 @@
 </head>
 
 <body class="background">
-    <form action="submit-form.php" class="form" method="POST">
+    <div class="body-container">
+        <div class="form-container">
+            <form action="submit-form.php" class="form" method="POST">
 
-        <h1 class="title">Sign up</h1>
+                <h1 class="title">Sign up</h1>
 
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username" placeholder="Username" required>
+                <label for="username" class="label">Username:</label>
+                <input type="text" id="username" name="username" placeholder="Username" required>
 
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" placeholder="Email" required>
+                <label for="email" class="label">Email:</label>
+                <input type="email" id="email" name="email" placeholder="Email" required>
 
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" placeholder="password" required>
+                <label for="password" class="label">Password:</label>
+                <input type="password" id="password" name="password" placeholder="password" required>
 
-        <label for="passwordrepeat">Repeat password</label>
-        <input type="password" id="passwordrepeat" name="passwordrepeat" placeholder="password" required>
-        <button type="button" onclick="location.href='/sign-up'">Sign up</button>
-    </form>
-    <div class="image-container">
-        <img src="{{ asset('images/sign-up.png') }}">
-    </div>
-    <div class="sign-switch">
-        <span class="button-label">Already have an Account?</span>
-        <button type="button" onclick="location.href='/sign-in'">Sign in</button><br><br>
+                <label for="passwordrepeat" class="label">Repeat password</label>
+                <input type="password" id="passwordrepeat" name="passwordrepeat" placeholder="password" required>
+                <button type="button" onclick="location.href='/sign-up'">Sign up</button>
+            </form>
+            <div class="sign-switch">
+                <span class="button-label">Already have an Account?</span>
+                <button type="button" onclick="location.href='/sign-in'">Sign in</button><br><br>
+            </div>
+        </div>
+        <div class="image-container">
+            <img src="{{ asset('images/sign-up.png') }}">
+        </div>
     </div>
 
 </body>
