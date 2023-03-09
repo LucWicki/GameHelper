1f2323057c46b1b0b6f4b74afda0e7752d572321
Luc Wicki <Luc_Wicki@sluz.ch>
2023-03-09 10:22:06 +0100

    Database shema created and trigger and stored procedures, Socrates and Luc

--- database/migrations/2023_03_09_071448_create_user_table.php
+++ database/migrations/2023_03_09_071448_create_user_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id('user_id');
             $table->timestamps();
 
+
             $table->string('firstname');
             $table->string('lastname');
             $table->string('username');
